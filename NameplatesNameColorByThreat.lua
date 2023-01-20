b538dbc058cccf78e0e792986af60968fcbcb173
defaultName <a@a.com>
2023-01-20 06:46:16 +0100

    First commit

---
+++ NameplatesNameColorByThreat.lua
@@ -0,0 +1,61 @@
+local frameName = "NameplatesNameColorByThreatFrame";
+local frame = _G[frameName];
+local isFrameExist = false;
+
+local function updateNameplatesNameColor(frame)
+    local unit = frame.displayedUnit;
+
+    if not unit or not unit:match('nameplate%d*') then return end;
+    if not C_NamePlate.GetNamePlateForUnit(unit) then return end;
+    if (not frame.name:IsShown()) then return end;
+    if (frame.UpdateNameOverride and frame:UpdateNameOverride()) then return end;
+    if (not ShouldShowName(frame)) then return end;
+    if (CompactUnitFrame_IsTapDenied(frame) or (UnitIsDead(unit) and not UnitIsPlayer(unit))) then return end;
+
+    local status = UnitThreatSituation("player", unit);
+    if (status) then
+        if (status > 0) then
+            frame.name:SetVertexColor(GetThreatStatusColor(status));
+        else
+            frame.name:SetVertexColor(1.0, 1.0, 1.0);
+        end
+    end
+end
+
+if (frame and frame:GetObjectType() == "Frame") then
+    isFrameExist = true;
+end
+
+if  (not isFrameExist) then
+    local frame = CreateFrame("Frame", frameName);
+    frame:RegisterEvent("PLAYER_ENTERING_WORLD");
+
+    local function onEvent(self, event, ...)
+        if (event == "PLAYER_ENTERING_WORLD") then
+            local initialLogin, reloadingUI = ...;
+            if (initialLogin or reloadingUI) then                
+                hooksecurefunc("CompactUnitFrame_UpdateName", updateNameplatesNameColor)
+
+                local trackThreatFrameName = "UpdateNameColorByThreatFrame";
+                local trackThreatFrame = _G[trackThreatFrameName];
+
+                if (not frame or frame:GetObjectType() ~= "Frame") then
+                    local trackThreatFrame = CreateFrame("Frame", trackThreatFrameName);
+                    trackThreatFrame:RegisterEvent("UNIT_THREAT_SITUATION_UPDATE");
+
+                    local function onEvent(self, event, ...)
+                        if (event == "UNIT_THREAT_SITUATION_UPDATE") then
+                            updateNameplatesNameColor(self);
+                        end
+                    end
+
+                    trackThreatFrame:SetScript("OnEvent", onEvent);
+                end
+
+                self:UnregisterAllEvents();
+            end
+        end
+    end
+
+    frame:SetScript("OnEvent", onEvent);
+end
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # NameplatesNameColorByThreat
+
+A very Lightweight addon to change the color of nameplates name based on the player's threat to the unit
+
+---
+
+### Colors in combat
+
+- White: not tanking, lower threat than tank.
+- Yellow: not tanking, higher threat than tank.
+- Orange: insecurely tanking, another unit have higher threat but not tanking.
+- Red: securely tanking, highest threat.
